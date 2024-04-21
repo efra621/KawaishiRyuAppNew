8803019b9aaf96156e066ccae67ba9f80d13bf07
EfrainMorales <eframorales421@gmail.com>
2024-04-21 06:30:25 -0300

    Agrego dependencias y verifico q funcionen Agrego ViewBinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
+    //kotlin("android")
+    //kotlin("android.extensions")
+    //kotlin("kapt")
 }
 
 android {
@@ -38,6 +41,9 @@ android {
         jvmTarget = "1.8"
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -51,6 +57,50 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     //Firebase
-//    implementation("com.google.firebase:firebase-analytics")
-//    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+
+    // Firebase Firestore, Storage, Auth
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-storage-ktx")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-auth")
+
+//    // Google Play Services Auth
+//    implementation("com.google.android.gms:play-services-auth")
+
+    // Kotlin
+    val n1 = "2.7.7"
+    implementation("androidx.navigation:navigation-fragment-ktx:$n1")
+    implementation("androidx.navigation:navigation-ui-ktx:$n1")
+
+    // Tools
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
+    implementation("de.hdodenhof:circleimageview:3.1.0") // Imagen circular
+    implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.hbb20:ccp:2.7.0") // InputNumber
+    implementation("com.airbnb.android:lottie:6.0.0") // Animation
+    implementation ("io.github.florent37:shapeofview:1.4.7")//ShapeOfView
+    implementation("me.relex:circleindicator:2.1.6") //Circle Indicator
+    implementation("org.imaginativeworld.whynotimagecarousel:whynotimagecarousel:2.1.0") //Carousel
+
+    // ViewModel and LiveData
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+
+    // ViewModel MVVM
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+
+    // Corrutinas
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.1")
+
+    //Implementation JSON
+    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+
+    //Datastore preference
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 }
--- app/src/main/java/app/gratum/kawaishiryuappnew/MainActivity.kt
+++ app/src/main/java/app/gratum/kawaishiryuappnew/MainActivity.kt
@@ -2,10 +2,19 @@ package app.gratum.kawaishiryuappnew
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import app.gratum.kawaishiryuappnew.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
 }
